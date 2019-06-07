bf11c68e55731e5de821013618c34daa888cd767
mraing <910408228@qq.com>
2019-06-07 21:22:39 +0800

    2019/06/07
    路由高亮状态未解决

--- vue-mi/src/components/home/childer/recommend.vue
+++ vue-mi/src/components/home/childer/recommend.vue
@@ -1,51 +1,148 @@
 <template>
-  <div id="recommend">
+  <div id="recommend" class="recommend">
     <!-- 轮播图 -->
     <ic-slider :autoplay="3000">
       <ic-slider-item v-for="(items,index) in images" :key="index">
-        <a :href="items.linkUrl"><img :src="items.picUrl"/></a>
+        <a :href="items.linkUrl">
+          <img :src="items.picUrl">
+        </a>
       </ic-slider-item>
     </ic-slider>
+
+    <!-- 快捷导航 -->
+    <ul class="common-model-list">
+      <li class="common-model-items" v-for="(commonMdeol,index) in commonModels" :key="index">
+        <a class="common-model-link" :href="commonMdeol.linkUrl">
+          <img :src="commonMdeol.imgUrl" :alt="commonMdeol.commonName">
+        </a>
+      </li>
+    </ul>
+
+    <!-- 热门商品 -->
+    <div class="hot-model">
+      <a href class="hot-model-left">
+        <img
+          src="http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c1e6980ae67a5484364a102761bd558b.jpg?thumb=1&w=358&h=508"
+        >
+      </a>
+      <div class="hot-model-right">
+        <a href class="hot-model-right-link">
+          <img
+            src="http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d6509933e37e8e21334de2628013cd4e.jpg?thumb=1&w=358&h=252"
+            alt
+          >
+        </a>
+        <a href class="hot-model-right-link">
+          <img
+            src="http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e5aa66e85ba2cf6248ee7e9709aba276.jpg?thumb=1&w=358&h=252"
+            alt
+          >
+        </a>
+      </div>
+    </div>
+
+    <!-- 热门商品横幅 -->
+    <a href class="hot-banner-link">
+      <img src="http://i8.mifile.cn/v1/a1/8ec0ba73-a08f-4093-acc6-6b5d2dcbb765!720x280.webp" alt>
+    </a>
+    <a href class="hot-banner-link">
+      <img src="http://i8.mifile.cn/v1/a1/7ef7313d-dd33-716a-fda6-8cc9417a5210!720x440.webp" alt>
+    </a>
 
   </div>
 </template>
 
 <script>
-import {IcSlider,IcSliderItem} from '../../../common/vue-better-slider'
+import { IcSlider, IcSliderItem } from "../../../common/vue-better-slider";
 export default {
   data() {
     return {
       recommend: "recommend",
       images: [
-          {
-            linkUrl: 'http://y.qq.com/w/album.html?albummid=0044K2vN1sT5mE',
-            picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000001YCZlY3aBifi.jpg',
-            id: 11351
-          },
-          {
-            linkUrl: 'https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=2197820&g_f=shoujijiaodian',
-            picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000004ckGfg3zaho0.jpg',
-            id: 11372
-          },
-          {
-            linkUrl: 'http://y.qq.com/w/album.html?albummid=001tftZs2RX1Qz',
-            picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M00000236sfA406cmk.jpg',
-            id: 11378
-          },
-          {
-            linkUrl: 'https://y.qq.com/msa/218/0_4085.html',
-            picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000001s0BXx3Zxcwb.jpg',
-            id: 11375
-          },
-          {
-            linkUrl: 'https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=2195876&g_f=shoujijiaodian',
-            picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000002cwng4353HKz.jpg',
-            id: 11287
-          }
-        ]
-    };  
+        {
+          linkUrl: "",
+          picUrl:
+            "http://i8.mifile.cn/v1/a1/def8e38c-54f4-5d57-95b6-5e7d21aaf528!720x360.webp",
+          id: ""
+        },
+        {
+          linkUrl: "",
+          picUrl:
+            "http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c9efb60f6f66f516ea26695ac744c2d9.jpg?thumb=1&w=720&h=360",
+          id: ""
+        },
+        {
+          linkUrl: "",
+          picUrl:
+            "http://i8.mifile.cn/v1/a1/80e4f91f-eeee-b4f0-1fd4-53892fa3a86c!720x360.webp",
+          id: ""
+        }
+      ],
+      commonModels: [
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/290976f3-acac-8a62-7e5c-a19985a6414b!144x152.webp",
+          commonName: "新品发布"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/e821d48d-57fa-7dee-ebc7-a836909cc8a7!144x152.webp",
+          commonName: "小米众筹"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/e8bc849a-0a3b-21a0-6810-7da3a3903dee!144x152.webp",
+          commonName: "以旧换新"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c94c523eb03128378ef2dc17c2c2cf0a.png?thumb=1&w=144&h=152",
+          commonName: "1分拼团"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/7dbcbf87-6a58-adb6-2f3f-bb3dae3e9c80!144x152.webp",
+          commonName: "超值特卖"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/cb65daec7ef7b52cc785f88f78efba37.png?thumb=1&w=144&h=152",
+          commonName: "小米秒杀"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/f400b464-f2cf-ada9-9048-e45dd699969a!144x152.webp",
+          commonName: "每日臻选"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/96c780016ea196743905dc93f9249c39.png?thumb=1&w=144&h=152",
+          commonName: "电视热卖"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://i8.mifile.cn/v1/a1/65fd2670-a567-dac3-59d1-6d96398cb733!144x152.webp",
+          commonName: "家电热卖"
+        },
+        {
+          linkUrl: "",
+          imgUrl:
+            "http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f11f9df6b0b0b428f8c8fc3267131830.png?thumb=1&w=144&h=152",
+          commonName: "米粉卡"
+        }
+      ]
+    };
   },
-  components:{
+  components: {
     IcSlider,
     IcSliderItem
   }
@@ -53,4 +150,72 @@ export default {
 </script>
 
 <style>
+.recommend{
+  background: #f5f5f5;
+}
+.common-model-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+.common-model-items {
+  position: relative;
+  width: 20%;
+  padding-top: 20%;
+  overflow: hidden;
+}
+.common-model-link {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.common-model-link > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.hot-model {
+  margin-top: 5px;
+  position: relative;
+  overflow: hidden;
+}
+
+.hot-model-left {
+  display: block;
+  width: 50%;
+  overflow: hidden;
+}
+.hot-model-left > img,
+.hot-model-right-link > img,
+.hot-banner-link > img {
+  width: 100%;
+  height: 100%;
+  vertical-align: bottom;
+  object-fit: cover;
+}
+.hot-model-right {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: calc(50% - 5px);
+}
+.hot-model-right-link {
+  display: block;
+  height: 50%;
+}
+.hot-model-right-link:first-child {
+  padding-bottom: 2.5px;
+}
+.hot-model-right-link:last-child {
+  padding-top: 2.5px;
+}
+.hot-banner-link {
+  display: block;
+  margin-top: 5px;
+  overflow: hidden;
+}
 </style>
+
+
--- vue-mi/src/components/home/home.vue
+++ vue-mi/src/components/home/home.vue
@@ -14,21 +14,22 @@
         <div class="home-hedaer-user iconfont icon-wd"></div>
       </div>
       <!-- 分类栏 -->
+
       <div class="home-category">
-        <router-link to="/home/recommend" class="home-category-items">推荐</router-link>
-        <router-link to="/home/activity" class="home-category-items">小米618</router-link>
-        <router-link to="/home/phone" class="home-category-items">手机</router-link>
-        <router-link to="/home/intelligent" class="home-category-items">智能</router-link>
-        <router-link to="/home/television" class="home-category-items">电视</router-link>
-        <router-link to="/home/notebook" class="home-category-items">笔记本</router-link>
-        <router-link to="/home/appliance" class="home-category-items">家电</router-link>
-        <router-link to="/home/lifearound" class="home-category-items">生活周边</router-link>
+        <router-link to="/home/recommend" class="home-category-items" exact>推荐</router-link>
+        <router-link to="/home/activity" class="home-category-items" exact>小米618</router-link>
+        <router-link to="/home/phone" class="home-category-items" exact>手机</router-link>
+        <router-link to="/home/intelligent" class="home-category-items" exact>智能</router-link>
+        <router-link to="/home/television" class="home-category-items" exact>电视</router-link>
+        <router-link to="/home/notebook" class="home-category-items" exact>笔记本</router-link>
+        <router-link to="/home/appliance" class="home-category-items" exact>家电</router-link>
+        <router-link to="/home/lifearound" class="home-category-items" exact>生活周边</router-link>
       </div>
     </header>
 
     <!-- 子页面切换 -->
     <div class="home-child-view">
-        <router-view></router-view>
+      <router-view keep-alive></router-view>
     </div>
   </div>
 </template>
@@ -44,6 +45,15 @@ export default {
 </script>
 
 <style>
+.home-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 75px;
+  z-index: 99;
+  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2);
+}
 .home-search {
   box-sizing: border-box;
   position: relative;
@@ -82,34 +92,36 @@ export default {
   border: 1px solid #eee;
   outline: none;
 }
-.home-category{
-    width: auto;
-    height: 30px;
-    background: #f2f2f2;
-    white-space: nowrap;
-    overflow-y: hidden;
-    overflow-x: auto;
+.home-category {
+  width: auto;
+  height: 30px;
+  background: #f2f2f2;
+  white-space: nowrap;
+  overflow-y: hidden;
+  overflow-x: auto;
 }
-.home-category-items{
-    display: inline-block;
-    margin: 0 5px;
-    padding: 0 5px; 
-    color: #999;
-    line-height: 28px;
-    font-size: 14px;
-    border-bottom: 2px solid transparent; 
-    text-decoration: none; 
+.home-category-items {
+  display: inline-block;
+  margin: 0 5px;
+  padding: 0 5px;
+  color: #999;
+  line-height: 28px;
+  font-size: 14px;
+  border-bottom: 2px solid transparent;
+  text-decoration: none;
 }
-.home-child-view{
-    position: absolute;
-    top: 75px;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    /* background: #ff6700; */
+.home-child-view {
+  position: absolute;
+  top: 75px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  max-width: 720px;
+  margin: 0 auto;
+  overflow: auto;
 }
-.home-category>.router-link-active{
-    color: #ff6700;
-    border-bottom: 2px solid #ff6700;
+.home-category > .router-link-active {
+  color: #ff6700;
+  border-bottom: 2px solid #ff6700;
 }
 </style>
