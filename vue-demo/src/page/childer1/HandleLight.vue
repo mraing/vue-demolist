7317202a058ed55ac753adbbb1034befec8c03cf
绪锋 <910408228@qq.com>
2019-08-06 14:02:58 +0800

    删除示例代码

--- vue-demo/src/page/childer1/HandleLight.vue
+++ vue-demo/src/page/childer1/HandleLight.vue
@@ -1,8 +1,10 @@
 <template>
  <div>
     <h5>{{title}}</h5>
-    <div v-if = "isShow">现在是开灯</div>
+    <!-- 运用 v-if 和 v-else -->
+    <div v-if="isShow">现在是开灯</div>
     <div v-else>现在是关灯</div>
+    <!-- 添加按钮事件 -->
     <button @click="handleLight">点击控制灯</button>
   </div>
 </template>
